b2f53d4393565a7d2cc67f8e439e7650ee4bff9a
Stanislav Iskortsev <iskortsevsv@gmail.com>
2021-07-02 12:57:22 +0200

    Create MyRESTController , add method showAllEmployees

---
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
@@ -0,0 +1,27 @@
+package com.zaurtregulov.spring.rest.controller;
+
+import com.zaurtregulov.spring.rest.entity.Employee;
+import com.zaurtregulov.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api") // стандарт в REST
+public class MyRESTController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    //показывает список работников
+    //в REST сервисе будет срабатывать метод showAllEmployees,
+    // spring с помощью функционала проекта Jackson
+    // конвектирует список работников в JSON формат
+    // в теле HTTP response содержится JSON который оторбразится в браузере
+    @GetMapping("/employees") // указываем в множественном числе
+    public List<Employee> showAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
