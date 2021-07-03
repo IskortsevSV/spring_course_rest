eb48505a2613ee2af64087c29f97b12e474ad44c
Stanislav Iskortsev <iskortsevsv@gmail.com>
2021-07-03 10:30:30 +0200

    Add deleteEmployee

--- src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
@@ -54,5 +54,21 @@ public class MyRESTController {
 
     }
 
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+
+        Employee employee = employeeService.getEmployee(id);
+
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " +
+                    id + " in Database");
+        }
+
+        employeeService.deleteEmployee(id);
+
+        return "Employee with ID = " + id + " was DELETED";
+
+    }
+
 
 }
